39277ec6248b65fe0bcdc0622940ab0c92525ad8
Aakash <aks.7d2000@gmail.com>
2025-02-21 22:10:57 +0530

    mlflow and dagshub created

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -1,47 +1,52 @@
-import os
 import pandas as pd
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from urllib.parse import urlparse
 import mlflow
 import mlflow.sklearn
-import numpy as np
 import joblib
-from src.entity.config_entities import ModelEvalConfig
-from src.utils.common import save_json
-from pathlib import Path
 import json
 
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from pathlib import Path
+from src.entity.config_entities import ModelEvalConfig
+import dagshub
 
-def save_json(path, data):
-    with open(path, "w") as f:
-        json.dump(data, f, indent=4)
+dagshub.init(repo_owner="aakash-tsx", repo_name="testing_auto_versioning", mlflow=True)
 
 
 class ModelEvaluation:
-    def __init__(self, config):
+    def __init__(self, config: ModelEvalConfig):
         self.config = config
+        mlflow.set_tracking_uri(
+            # "http://localhost:5000"
+            "https://dagshub.com/aakash-tsx/testing_auto_versioning.mlflow"
+        )  # Change if using remote MLflow
+        mlflow.set_experiment("Model_Evaluation")
 
     def eval_metrics(self, actual, pred):
-        rmse = np.sqrt(mean_squared_error(actual, pred))
-        mae = mean_absolute_error(actual, pred)
-        r2 = r2_score(actual, pred)
-        return {"rmse": rmse, "mae": mae, "r2": r2}
+        return {
+            "rmse": np.sqrt(mean_squared_error(actual, pred)),
+            "mae": mean_absolute_error(actual, pred),
+            "r2": r2_score(actual, pred),
+        }
 
     def log_evaluation(self):
         test_data = pd.read_csv(self.config.test_data)
         model = joblib.load(self.config.model_path)
 
-        test_x = test_data.drop([self.config.target_column], axis=1)
+        test_x = test_data.drop(columns=[self.config.target_column])
         test_y = test_data[self.config.target_column]
 
-        predicted_qualities = model.predict(test_x)
+        preds = model.predict(test_x)
+        metrics = self.eval_metrics(test_y, preds)
 
-        # Compute evaluation metrics
-        metrics = self.eval_metrics(test_y, predicted_qualities)
-        print(metrics["rmse"])
-        print(metrics["mae"])
-        print(metrics["r2"])
+        print(
+            f"RMSE: {metrics['rmse']:.4f}, MAE: {metrics['mae']:.4f}, R²: {metrics['r2']:.4f}"
+        )
+
+        with mlflow.start_run():
+            mlflow.log_params({"model_name": self.config.model_path})
+            mlflow.log_metrics(metrics)
+            mlflow.sklearn.log_model(model, "evaluated_model")
 
         # Save metrics locally
-        save_json(path=Path(self.config.metric_file), data=metrics)
-
+        Path(self.config.metric_file).write_text(json.dumps(metrics, indent=4))
